b96848644f6c36b99f6a661bba3bea844d9fced0
Brent Bergwall <kothx@yahoo.com>
2021-02-20 18:52:31 -0600

    use 1.16 embed  feature for templates

--- taskshandler/templates.go
+++ taskshandler/templates.go
@@ -1,50 +1,26 @@
 package taskshandler
 
 import (
+	"embed"
+	"io/fs"
+
 	"github.com/blbgo/httpserver"
 )
 
+//go:embed templates
+var templatesFS embed.FS
+
 type templates struct{}
 
-// NewTemplates provides the templates needed by the log handler
-func NewTemplates() httpserver.TemplateProvider {
+// NewTemplateFS provides the templates
+func NewTemplateFS() httpserver.TemplateFSProvider {
 	return templates{}
 }
 
-// these templates assume "header" and "footer" templates are defined elsewhere
-var templateString = `
-{{define "tasks"}}
-{{template "header"}}
-<h2>Tasks</h2>
-<ul class="list pl3">
-	{{range $i, $e := . -}}
-		<li class="pb2"><a href="/task/input/{{$i}}" class="dim link near-white b">{{.Name}}</a></li>
-	{{- end}}
-</ul>
-{{template "footer"}}
-{{end}}
-
-{{define "taskInputs"}}
-{{template "header"}}
-<h2>Run task "{{.Name}}"</h2>
-<form action="/task/run" method="post">
-	<input type="hidden" name="id" value="{{.ID}}">
-	{{range $i, $e := .Inputs -}}
-		<input class="input-reset ba br3 b--mid-gray pa2 mb3 db w-100 outline-0" placeholder="{{.}}" type="text" name="I{{$i}}">
-	{{- end}}
-	<input type="submit" value="Run">
-</form>
-{{template "footer"}}
-{{end}}
-
-{{define "task"}}
-{{template "header"}}
-<h2>Task</h2>
-<h3>Task "{{.Name}}" has been started.</h3>
-{{template "footer"}}
-{{end}}
-`
-
-func (r templates) Template() string {
-	return templateString
+func (r templates) TemplateFS() (fs.FS, error) {
+	subFS, err := fs.Sub(templatesFS, "templates")
+	if err != nil {
+		return nil, err
+	}
+	return subFS, nil
 }
